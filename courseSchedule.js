7087f96d84a26ba319d3db82b69acdf8ab36caa2
brian-quach <briiquach@gmail.com>
2018-12-01 13:12:00 -0500

    get timetable w/ id + campus

--- courseSchedule.js
+++ courseSchedule.js
@@ -21,9 +21,10 @@ function courseToString(course){
 }
 
 function printCourseResults(courses){
-    let out = [];
+    // Makes printing less ugly
+    let out = {};
     for (i = 0; i < courses.length; i++) {
-        out.push(courseToString(courses[i]));
+        out[courses[i].id] = courseToString(courses[i]);
     }
     return out;
 }
@@ -35,9 +36,9 @@ app.get('/api/getCourses/:page', function (req, res, next) {
 
     let pgNum = req.params.page;
     let skip = (parseInt(pgNum)-1)*10;
-
     url += "&skip=" + skip;
-        Request.get(url, (error, response, body) => {
+
+    Request.get(url, (error, response, body) => {
         if(error) {
             return console.dir(error);
         }
@@ -45,7 +46,34 @@ app.get('/api/getCourses/:page', function (req, res, next) {
     });
 });
 
+app.get('/api/timetable/:courseid/:campus/:term/', function (req, res, next){
+    let url = API;
+    url += "courses/";
+    url += "?key=" + API_KEY;
 
+    let courseId = req.params.courseid;
+    url += "&id=" + courseId;
+    let campus = req.params.campus;
+    let term = (req.params.term).slice(0, 4) + " " + (req.params.term).slice(4);
+
+    console.log(url);
+
+    Request.get(url, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        let result = JSON.parse(body);
+
+        for (i = 0; i < result.length; i++) {
+            if ((result[i].term == term) && (result[i].campus == campus)){
+                let courseTimes = result[i].meeting_sections;
+                return res.json(courseTimes);
+            }
+        }
+        res.json("Could not find course");
+    });
+
+})
 
 app.use(function (req, res, next){
     console.log("HTTP request", req.method, req.url, req.body);
